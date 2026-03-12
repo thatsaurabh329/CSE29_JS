7331a1818489921963b570062c4ec6be95f02a05
thatsaurabh329 <thatsaurabh329@gmail.com>
2026-03-12 23:41:38 +0530

    Conditional statements

---
+++ CLASS/lec4b.js
@@ -0,0 +1,38 @@
+
+//IF STATEMENT
+
+var age = 20;
+if (age > 18) {
+    document.write("<b>You are eligible to cast the vote.</b>");
+}
+
+// IF- ELSE STATEMENT
+
+let age = 20;
+if (age > 18) {
+    document.write("<b>You are  audlt </b>");
+}
+else {
+    document.write(" you are not audlt")
+}
+
+//switch case
+
+var grade = 'A';
+
+document.write("Entering switch block<br />");
+
+switch (grade) {
+
+case 'A': document.write("Good job<br />"); break;
+
+case 'B': document.write("Pretty gpod<br />"); break;
+
+case 'C': document.write("Passed<br />"); break;
+
+case 'D': document.write("Not so good<br />"); break;
+
+case 'F': document.write("Failed<br />"); break;
+
+default: document.write("Unknown grade<br />")
+}
