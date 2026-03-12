5f244978563d35be97691ae62a97c692427ae6a8
thatsaurabh329 <thatsaurabh329@gmail.com>
2026-03-12 23:49:01 +0530

    JavaScript - function

---
+++ CLASS/lec4c.js
@@ -0,0 +1,18 @@
+
+//JAVA SCRIPT FUNCTION W/O PARAMETERS
+function myFunction()
+ {
+     alert("Hello World!");
+}
+     
+//JAVA SCRIPT FUNCTION WITH PARAMETERS
+function sayHello(name, age) {
+
+document.write (name+" is"+age+" years old.");
+}
+//FUNCTION RETURN
+var res= mul( 12,30);
+function mul(x,y){
+     return x*y;
+}
+document.write(res);
